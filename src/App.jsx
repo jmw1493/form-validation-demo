01c57341de9e8adc9015437e04273d5ec5ed6fed
Jared <weissjmw@gmail.com>
2023-04-05 13:24:30 -0500

    error validation for name working

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
   const [year, setYear] = useState("");
   const [cvc, setCvc] = useState("");
   const [formSubmitted, setFormSubmitted] = useState(false);
+  const [errors, setErrors] = useState(new Set());
 
   return (
     <div className="container">
@@ -53,6 +54,8 @@ const App = () => {
             cvc={cvc}
             setCvc={setCvc}
             setFormSubmitted={setFormSubmitted}
+            errors={errors}
+            setErrors={setErrors}
           />
         </div>
       )}
